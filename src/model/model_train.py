53761ede23583f355ecb6f538c935475fe892279
JoyAlbertini <43403510+JoyAlbertini@users.noreply.github.com>
2024-10-24 00:03:17 +0200

    small improvements

--- src/model/model_train.py
+++ src/model/model_train.py
@@ -7,6 +7,7 @@ from src.utils import utils
 
 
 def model_train(only_recent_issues: bool, use_gpu: bool = True, batch_size: int = 16, epochs: int = 5, lr: float = 2e-5):
+    print(f"Training model with only recent issues: {only_recent_issues}")
     models_dir = get_chosen_model_dir(only_recent_issues)
     predictor = Predictor(models_dir, use_gpu, batch_size, epochs, lr)
     df = Database.get_train_set() if not only_recent_issues else Database.get_recent_instances()
